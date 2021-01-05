0d8c77fcfc0beb99e4d033be7c2fcaa4db99aa7a
AlboOdelia <alboodelia@gmail.com>
2021-01-05 23:33:19 +0200

    COPY ALL CLASS FROM PREV GIT

---
+++ DS/DataSource.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Location;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DO;
+
+
+namespace DS
+{
+    public static class DataSource
+    {
+        public static List<BusLine> BusLineList;
+        public static List<BusLineStation> BusLineStationList;
+        public static List<BusStation> BusStationList;
+        public static List<User> UserList;
+        public static List<ConsecutiveStations> ConsecutiveStationsList;
+        public static Random r = new Random();
+        static DataSource()
+        {
+            BusLineStationList = new List<BusLineStation>();
+            BusStationList = new List<BusStation>();
+            UserList = new List<User>();
+        }
+        static void InitAllLists()
+        {
+            BusStationList = new List<BusStation>();
+            for (int i = 0; i < 50; i++)
+            {
+                BusStationList.Add(new BusStation
+                {
+                    BusStationKey = RunNumbers.BusStationRunNumber++,
+                    Coordinates = new GeoCoordinate(r.NextDouble() * (33.3 - 31) + 31, r.NextDouble() * (35.5 - 34.3) + 34.3)
+                });
+            }
+            BusLineList = new List<BusLine>();
+            for (int i = 0; i < 12; i++)
+            {
+                BusLineList.Add(new BusLine
+                {
+                    BusLineKey = RunNumbers.BusLineRunNumber++,
+                    LineNumber = r.Next(1000000, 10000000),
+                    Area = (Areas)r.Next(0, 5),
+                    IsActive = true
+                });
+            }
+        }
+    }
+}
---
+++ DalObject/Cloning.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+//using DO;
+namespace DL
+{
+    static class Cloning
+    {
+        internal static T Clone<T>(this T original) where T : new()
+        {
+            T copyToObject = new T();
+            foreach (PropertyInfo propertyInfo in typeof(T).GetProperties())
+                propertyInfo.SetValue(copyToObject, propertyInfo.GetValue(original, null), null);
+
+            return copyToObject;
+        }
+    }
+}
