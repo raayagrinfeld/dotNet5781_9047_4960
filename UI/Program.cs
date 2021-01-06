e3e98276ad40ae634ebd1acc08da7285da1b1a01
AlboOdelia <alboodelia@gmail.com>
2021-01-06 19:11:15 +0200

    djsndwl

--- UI/Program.cs
+++ UI/Program.cs
@@ -16,15 +16,22 @@ namespace PlConsole
         {
             bl = BlFactory.GetBl("1");
             BusLineBO busLineBOs = bl.GetBusLine(20000);
-            bl.AddStation(busLineBOs, 30080);
+            try
+            {
+                bl.AddStation(busLineBOs, 30080);
+            }
+            catch(BO.BadBusStationKeyException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
             IEnumerable<BusLineStationBO> b = busLineBOs.busLineStations;
             for(int i=0;i< b.Count();i++)
             {
                 Console.WriteLine(b.ElementAt(i));
             }
-            bl.deleteBusStationInBusLine(busLineBOs[0], busLineBOs[0].FirstStation);
-            Console.WriteLine("delete first station:");
-            Console.WriteLine(busLineBOs[0]);
+            //bl.deleteBusStationInBusLine(busLineBOs[0], busLineBOs[0].FirstStation);
+            //Console.WriteLine("delete first station:");
+            //Console.WriteLine(busLineBOs[0]);
         }
     }
 }
